349ffb0ac31e32336d94d713081620950cdd1ca4
lolczak <lukasz.jan.olczak@gmail.com>
2015-06-03 19:49:21 +0200

    refactoring - full file printed

--- src/main/scala/org/lolczak/alacarte/approach1/io/IoApp.scala
+++ src/main/scala/org/lolczak/alacarte/approach1/io/IoApp.scala
@@ -1,12 +1,14 @@
 package org.lolczak.alacarte.approach1.io
 
-import org.lolczak.alacarte.approach1.calculator.{Pure, Term}
+import org.lolczak.alacarte.approach1.calculator.{TermInstances, Pure, Term}
 import org.lolczak.alacarte.approach1.control._
 import ExecInstances._
 import TeletypeInstances._
 import FileSystemInstances._
 import org.lolczak.alacarte.approach1.control.InjectInstances._
 import org.lolczak.alacarte.approach1.control.CoproductInstances._
+import scalaz.std.AllInstances._
+
 
 object IoApp extends App {
 
@@ -16,10 +18,11 @@ object IoApp extends App {
 
   def putChar[F[_]](char: Char)(implicit I0: Teletype :<: F): Term[F, Unit] = Term.inject[Teletype, F, Unit](PutChar(char, Pure()))
 
+
   def cat(path: FilePath): Term[T, Int] = for {
     content <- readFile[T](path)
     size = content.length
-    _ <- putChar[T](content.charAt(0))
+    _ <- TermInstances.monad[T].traverse[Char, List, Unit](content.toList)(putChar[T](_))
   } yield size
 
   val size = Exec.exec[T,Int](cat("/Users/lolczak/.profile")).unsafePerformIO()
