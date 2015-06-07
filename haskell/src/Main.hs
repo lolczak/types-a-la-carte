e02689a402a40e3b984aa7f465166c63909953eb
lolczak <lukasz.jan.olczak@gmail.com>
2015-06-07 17:33:18 +0200

    refactoring

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -8,4 +8,4 @@ import Expr
 addExample :: Expr(Val :+: Add)
 addExample = In(Inr(Add (In(Inl(Val 118))) (In(Inl(Val 1219)) )))
 
-main = putStrLn ("addExample: " ++ show (eval addExample))
+main = putStrLn $ "addExample: " ++ (show $ eval addExample)
